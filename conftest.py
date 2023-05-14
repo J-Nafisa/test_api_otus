35fd4e19922b57829e8cd981ff3dfe131dd21eea
J-Nafisa <131007811+J-Nafisa@users.noreply.github.com>
2023-05-15 02:20:05 +0300

    conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="https://ya.ru")
     parser.addoption("--status_code", action="store", type=int, default=200)
@@ -12,4 +11,3 @@ def url(request):
 @pytest.fixture
 def status_code(request):
     return request.config.getoption("--status_code")
-
